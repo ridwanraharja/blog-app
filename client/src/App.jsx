cbfa417509d42ecff9e27c563db2306694ff3cde
ridwanraharja <ridwanraharja17@gmail.com>
2024-02-02 16:14:01 +0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { About } from "./pages/About";
+import { Projects } from "./pages/Projects";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="text-3xl text-red-400">App</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
