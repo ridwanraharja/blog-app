af3aa747dfaec723e1a575f485187eaa9bcf11e6
ridwanraharja <ridwanraharja17@gmail.com>
2024-02-04 00:33:37 +0700

    create skeleton signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 export const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="h-[80vh] flex items-center justify-center">
+      <form className="flex max-w-md flex-col gap-4">
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email1" value="Your email" />
+          </div>
+          <TextInput
+            id="email1"
+            type="email"
+            placeholder="name@flowbite.com"
+            required
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password1" value="Your password" />
+          </div>
+          <TextInput id="password1" type="password" required />
+        </div>
+        <div className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Label htmlFor="remember">Remember me</Label>
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
